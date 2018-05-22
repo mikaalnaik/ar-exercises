eb46cd87aaa0d8325b8d03295dc04c5a62983c8f
Mikaal Naik <matthewnaik@gmail.com>
2018-05-22 18:32:13 +0000

    Ex. 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+
+@store2 = Store.find(2)
+@store3 = Store.find(3)
+
+puts @store1.name, @store2.name, @store3.name
+
+@store2.name = 'Aritiza'
+
+puts @store1.name, @store2.name, @store3.name
